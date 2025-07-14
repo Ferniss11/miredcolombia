4425bb5933a0a358b716053edf71904b9080dc58
Andrés Gómez <innovacioninteligente.ia@gmail.com>
2025-07-14 00:14:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { cn } from '@/lib/utils';
@@ -19,16 +18,6 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
-        <Script id="website-ownership-verification" strategy="afterInteractive" data-noptimize="1" data-cfasync="false" data-wpfc-render="false">
-          {`
-            (function () {
-                var script = document.createElement("script");
-                script.async = 1;
-                script.src = 'https://mn-tz.com/NDM1OTUy.js?t=435952';
-                document.head.appendChild(script);
-            })();
-          `}
-        </Script>
       </head>
       <body className={cn("min-h-screen bg-background font-body antialiased")}>
         <AuthProvider>
