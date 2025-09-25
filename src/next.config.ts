4a46e1c528c6c2cc7ccd9ae524b45bebb5aa9991
Andrés Gómez <innovacioninteligente.ia@gmail.com>
2025-09-25 10:27:13 +0000

    Aquí está el error completo:
    Respuesta de la Herramienta
    [ERROR: La he

--- src/next.config.ts
+++ src/next.config.ts
@@ -49,6 +49,10 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: "https",
+        hostname: "**.googleusercontent.com",
       }
     ],
   },
